060e56b5aceadc4107aeb964de88d329b9466a2a
ozumoo <ozumoo@hotmail.com>
2019-07-08 12:42:13 +0200

    Finish blog with posts and comments

--- Clean-Blog/db/schema.rb
+++ Clean-Blog/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190626134817) do
+ActiveRecord::Schema.define(version: 20190707235334) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
